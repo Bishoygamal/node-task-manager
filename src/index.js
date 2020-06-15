d754408b867932ec5274a08d6092b729ccc67622
Beshoy Gamal <beshoy.gamal@itworx.com>
2020-06-15 19:45:14 +0200

    Api

--- src/index.js
+++ src/index.js
@@ -37,11 +37,16 @@ pet.toJSON = function(){
 console.log(JSON.stringify(pet))
 
 const Task = require("./models/task");
+const User = require("./models/user")
 
 const main = async ()=>{
-    const task = await Task.findById('5ee724cf85b3ed7c90385e07');
-    await task.populate('owner').execPopulate()
-    console.log(task.owner);
+    // const task = await Task.findById('5ee724cf85b3ed7c90385e07');
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner);
+
+    const user = await User.findById('5ee73744505c72451044e031')
+    await user.populate('tasks').execPopulate()
+    console.log(user.tasks)
 }
 
 main();
--- src/routers/task.js
+++ src/routers/task.js
@@ -17,20 +17,22 @@ router.post('/tasks',auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth,async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        // const tasks = await Task.find({})
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id',auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        // const task = await Task.findById(_id)
+        const task = await Task.findOne({_id,owner:req.user._id})
 
         if (!task) {
             return res.status(404).send()
@@ -42,7 +44,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id',auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -52,14 +54,16 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({_id:req.params.id,owner:req.user._id})
+       // const task = await Task.findById(req.params.id)
 
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
+ 
 
         if (!task) {
             return res.status(404).send()
         }
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
 
         res.send(task)
     } catch (e) {
@@ -67,10 +71,11 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth,async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
+        const task = await Task.findOneAndDelete({_id:req.params.id,owner:req.user._id})
+        //const task = await Task.findByIdAndDelete(req.params.id)
+  
         if (!task) {
             res.status(404).send()
         }
