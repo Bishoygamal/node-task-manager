5c776c471afada9128f2521617c01db800e62399
Beshoy Gamal <beshoy.gamal@itworx.com>
2020-06-17 21:33:30 +0200

    File upload

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,12 +3,12 @@ const Task = require('../models/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/tasks',auth, async (req, res) => {
-   // const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        owner:req.user._id
+        owner: req.user._id
     })
+
     try {
         await task.save()
         res.status(201).send(task)
@@ -17,22 +17,28 @@ router.post('/tasks',auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', auth,async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
+    const match ={};
+
+    if(req.query.completed){
+        match.completed = req.query.completed === 'true'
+    }
     try {
-        // const tasks = await Task.find({})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path:'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
 })
 
-router.get('/tasks/:id',auth, async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        // const task = await Task.findById(_id)
-        const task = await Task.findOne({_id,owner:req.user._id})
+        const task = await Task.findOne({ _id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
@@ -44,7 +50,7 @@ router.get('/tasks/:id',auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id',auth, async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -54,28 +60,24 @@ router.patch('/tasks/:id',auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({_id:req.params.id,owner:req.user._id})
-       // const task = await Task.findById(req.params.id)
-
- 
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
 
         if (!task) {
             return res.status(404).send()
         }
+
         updates.forEach((update) => task[update] = req.body[update])
         await task.save()
-
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/tasks/:id', auth,async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findOneAndDelete({_id:req.params.id,owner:req.user._id})
-        //const task = await Task.findByIdAndDelete(req.params.id)
-  
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+
         if (!task) {
             res.status(404).send()
         }
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -52,23 +53,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-router.patch('/users/me',auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -78,33 +63,37 @@ router.patch('/users/me',auth, async (req, res) => {
     }
 
     try {
-        // const user = await User.findById(req.params.id)
-
         updates.forEach((update) => req.user[update] = req.body[update])
         await req.user.save()
-
-        // if (!user) {
-        //     return res.status(404).send()
-        // }
-
         res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/me',auth, async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        // const user = await User.findByIdAndDelete(req.user._id)
-
-        // if (!user) {
-        //     return res.status(404).send()
-        // }
         await req.user.remove()
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
+})
+
+const upload = multer({
+    dest:'avatars',
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,cb){
+        
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined,true)
+
+    }
+})
+router.post('/users/me/avatar',upload.single('avatar'),(req,res)=>{
+res.send()
 })
 
 module.exports = router
